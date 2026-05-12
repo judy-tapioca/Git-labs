b969ac307a7d46eeacf284ab5bbcdd33c798d1cc
Judy Banda <judybanda19@gmail.com>
2026-05-12 19:22:07 +0300

    Final lab5 report

---
+++ cmake_lab/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Judy Banda/Git-labs/cmake_lab
+# Build directory: C:/Users/Judy Banda/Git-labs/cmake_lab/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
