be8d1a0bf70b8b0c2b30b6fb9621c05fd86a9ccd
Judy Banda <judybanda19@gmail.com>
2026-05-12 19:45:02 +0300

    Fix: Remove local build artifacts for Lab 6

--- cmake_lab/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Judy Banda/Git-labs/cmake_lab
-# Build directory: C:/Users/Judy Banda/Git-labs/cmake_lab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
